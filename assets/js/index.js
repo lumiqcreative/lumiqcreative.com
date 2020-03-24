ac2e73260a27c2f705be06ad2791875ec351cb4a
Lumiq Creative <contact@lumiqcreative.com>
2020-03-24 17:37:21 +0100

    Redesign Interface

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,20 @@
-$("#colr-mode-toggle").on("click", function() {
-    if($("body").hasClass("is-dark")) {
-      $("body").removeClass("is-dark");
-      $("#ic-colr-mode").removeClass("is-rotated");
-    } else {
-      $("body").addClass("is-dark");
-      $("#ic-colr-mode").addClass("is-rotated");
+/* function MHhV() {
+    var a = document.getElementById("gwSD").clientWidth - 48;
+    var b = document.getElementById("KyEB").clientWidth;
+    var c = document.getElementById("yeDW").offsetWidth;
+    var d = document.getElementById("gwSD");
+    var e = document.getElementById("yeDW");
+
+    if (b + c + 36 >= a) {
+        d.classList.add("oPRx");
+        e.classList.add("WXTq");
+    } 
+    
+    else {
+        d.classList.remove("oPRx");
+        e.classList.remove("WXTq");
     }
-});
+}
+
+window.onresize = MHhV;
+window.onload = MHhV; */
