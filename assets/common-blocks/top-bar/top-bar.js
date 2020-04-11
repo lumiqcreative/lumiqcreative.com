1580ebbfa8c8c73a6e5732c18e277e3920441d6b
Lumiq Creative <contact@lumiqcreative.com>
2020-04-11 18:31:27 +0200

    Refactor Code & Add LiftApp Case Study

--- assets/common-blocks/top-bar/top-bar.js
+++ assets/common-blocks/top-bar/top-bar.js
@@ -1,9 +1,9 @@
 document.querySelector(".top-bar__menu-icon").onclick = function() {
     if(document.querySelector(".top-bar__inner-menu-drawer").classList.contains("top-bar__inner-menu-drawer_open")) {
         document.querySelector(".top-bar__inner-menu-drawer").classList.remove("top-bar__inner-menu-drawer_open");
-        document.querySelector(".top-bar__menu-icon-img").src = "assets/img/menu.svg";
+        document.querySelector(".top-bar__menu-icon-img").src = "/assets/img/menu.svg";
     } else {
         document.querySelector(".top-bar__inner-menu-drawer").classList.add("top-bar__inner-menu-drawer_open");
-        document.querySelector(".top-bar__menu-icon-img").src = "assets/img/x.svg";
+        document.querySelector(".top-bar__menu-icon-img").src = "/assets/img/x.svg";
     }
 };
