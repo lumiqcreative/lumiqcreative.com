ff4e87c43fb80efc2500dac4b8458920efb3df62
Lumiq Creative <contact@lumiqcreative.com>
2020-04-13 20:44:50 +0200

    Refine Top Bar & Update LiftApp Case Study

--- assets/common-blocks/top-bar/top-bar.js
+++ assets/common-blocks/top-bar/top-bar.js
@@ -1,9 +1,9 @@
-document.querySelector(".top-bar__menu-icon").onclick = function() {
+document.querySelector(".top-bar__menu-button").onclick = function() {
     if(document.querySelector(".top-bar__inner-menu-drawer").classList.contains("top-bar__inner-menu-drawer_open")) {
         document.querySelector(".top-bar__inner-menu-drawer").classList.remove("top-bar__inner-menu-drawer_open");
-        document.querySelector(".top-bar__menu-icon-img").src = "/assets/img/menu.svg";
+        document.querySelector(".top-bar__menu-icon").classList.remove("top-bar__menu-icon_clicked");
     } else {
         document.querySelector(".top-bar__inner-menu-drawer").classList.add("top-bar__inner-menu-drawer_open");
-        document.querySelector(".top-bar__menu-icon-img").src = "/assets/img/x.svg";
+        document.querySelector(".top-bar__menu-icon").classList.add("top-bar__menu-icon_clicked");
     }
 };
