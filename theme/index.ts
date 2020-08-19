a121a24bf0b4ae763094876eb4c047f3a556db8f
Lumiq Creative <contact@lumiqcreative.com>
2020-08-19 16:28:31 +0200

    Update Colours

--- theme/index.ts
+++ theme/index.ts
@@ -1,9 +1,9 @@
 const colors = {
-  accent: 'hsl(264, 100%, 50%)',
+  accent: 'hsl(256, 100%, 56%)',
   surface: 'hsl(0, 0%, 100%)',
   onSurface: 'hsl(0, 0%, 0%)',
-  onSurfaceMedium: 'hsl(264, 16%, 64%)',
-  border: 'hsl(264, 16%, 88%)'
+  onSurfaceMedium: 'hsl(240, 16%, 64%)',
+  border: 'hsl(240, 16%, 88%)'
 }
 
 const breakpoints = [720, 1080]
