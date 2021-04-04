4d1a7fc97daa7f78ad85d801081af9634ae49341
Patryk Michalik <contact@patrykmichalik.com>
2021-04-04 19:43:42 +0200

    Set up redirects for old pages

--- next.config.js
+++ next.config.js
@@ -5,5 +5,19 @@ module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   future: {
     webpack5: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/deliverables/overdrop-blocks',
+        destination: '/posts/overdrop-mockups',
+        permanent: true
+      },
+      {
+        source: '/resources/device-frames',
+        destination: '/posts/device-frames',
+        permanent: true
+      }
+    ]
   }
 })
